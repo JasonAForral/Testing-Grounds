33b186bd979d2fdc1a92072b35b155a3441d40e0
Tsinoyboi <ajsnoface@gmail.com>
2015-05-14 04:11:22 -0700

    Sampling ScriptedObjects
    and stuff

---
+++ Assets/_Scripts/CharacterSheet.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+ 
+[System.Serializable]
+public class CharacterSheet
+{
+ 
+    //======================================
+    // static variables
+    
+    // public variables
+    public string objectName = "Generic Character";
+    public bool isBool = true;
+    public Vector3 recentPosition;
+    
+    // private variables
+ 
+    //======================================
+    // Function Definitions
+ 
+    // getters & setters
+
+    // member functions
+}
--- Assets/_Scripts/LookAt.cs
+++ Assets/_Scripts/LookAt.cs
@@ -3,11 +3,16 @@ using System.Collections;
 
 public class LookAt : MonoBehaviour {
 
-    public Transform target;
+    [SerializeField]
+    private Transform target;
 
-	// Update is called once per frame
-	void Update () {
-        transform.LookAt(target);
-	
-	}
+    [SerializeField]
+    private float factor = 2f;
+
+    void Update ()
+    {
+        Quaternion targetRotation = Quaternion.LookRotation(target.position - transform.position + Vector3.up);
+        if (transform.rotation != targetRotation)
+            transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, factor * Time.deltaTime);
+    }
 }
